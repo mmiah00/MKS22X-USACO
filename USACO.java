434bea7889aa1dc21c57460dd44f83dbc2f1737d
mmiah00 <mmiah00@stuty.edu>
2019-03-06 14:35:02 -0500

    started making a class to keep track of lake making

--- USACO.java
+++ USACO.java
@@ -1,3 +1,36 @@
+import java.util.*;
+import java.io.*;
+
 public class USACO {
+
+  public static int bronze(String filename){
+    return 1;
+  }
+
+  private class makeLake {
+    private int elevation;
+    private int numInstructions;
+    private int[][] pasture;
+
+    public makeLake (int r, int c, int e, int n) {
+      pasture = new int [r][c];
+      elevation = e;
+      numInstructions = n;
+    }
+
+
+  }
+
+  public static int silver(String filename) {
+    return 1;
+  }
+
+  private ArrayList <String> readFile  (String filename) {
+    Scanner inf = new Scanner (filename);
+    while (inf.hasNextLine ()) {
+      String line = inf.nextLine ();
+    }
+    return new ArrayList <String> ();
+  }
 
 }
