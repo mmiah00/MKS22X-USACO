4f2db1a02dfb60408d6426e2d1bbbb137c59beb3
mmiah <mmiah00@stuy.edu>
2019-03-07 22:54:43 -0500

    finding lines works

--- USACO.java
+++ USACO.java
@@ -1,37 +1,40 @@
 import java.util.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class USACO {
   public static int bronze(String filename){
     return 1;
   }
 
-  private static String [] instructions (String filename) {
-    Scanner inf = new Scanner (filename);
-    return inf.split ("\n");
-    /*
-    n[0] = inf.nextInt ();
-    n[1] = inf.nextInt ();
-    n[2] = inf.nextInt ();
-    n[3] = inf.nextInt ();
-    inf.close ();
-    return n;
-    */
+  private static int [] sizes (String filename) throws FileNotFoundException {
+    ArrayList <String> instructions = readFile (filename);
+    //String [] sizes = new String [4];
+    String firstLine = instructions.get (0);
+    String[] nums = firstLine.split (" ");
+    int [] sizes = new int [4];
+    for (int i = 0; i < 4; i ++) {
+      sizes[i] = Integer.parseInt (nums[i]);
+    }
+    return sizes;
   }
 
-  private ArrayList<String> readFile (String filename) {
-    Scanner inf = new Scanner (filename);
+  private static ArrayList<String> readFile (String filename) throws FileNotFoundException {
+    File f = new File (filename);
+    Scanner inf = new Scanner (f);
+    ArrayList <String> lines = new ArrayList <String> ();
     while (inf.hasNextLine ()) {
-      String line = inf.nextLine ();
+      String line = inf.nextLine();
+      lines.add (line);
     }
-    return new ArrayList <String> ();
+    return lines;
   }
 
   public static int silver(String filename) {
     return 1;
   }
 
-  public static void toString (String [] s) {
+  public static void toString (int [] s) {
     String ans = "";
     for (int x = 0; x < s.length; x ++) {
       System.out.println (s[x]);
@@ -39,6 +42,12 @@ public class USACO {
   }
 
   public static void main (String[] args) {
-    toString (instructions ("makelake.in"));
+    //toString (instructions ("makelake.in"));
+    try {
+      toString (sizes("makelake.in"));
+    }
+    catch (Exception e) {
+      System.out.println ("file not found");
+    }
   }
 }
